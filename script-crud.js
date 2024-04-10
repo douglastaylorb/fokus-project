b9e54906164a5e23be5dbe0e1d6fd21d94914266
Douglas Taylor <78963489+douglastaylorb@users.noreply.github.com>
2024-04-10 19:42:33 -0300

    Permite alterar e selecionar lista de tarefas

--- script-crud.js
+++ script-crud.js
@@ -6,6 +6,10 @@ const btnCancelarTarefa = document.querySelector(".app__form-footer__button--can
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 
+const paragrafoDescricaoTarefa = document.querySelector(".app__section-active-task-description")
+let tarefaSelecionada = null
+let liTarefaSelecionada = null
+
 
 function atualizarTarefas(){
     localStorage.setItem('tarefas', JSON.stringify(tarefas))
@@ -47,6 +51,24 @@ function criarElementoTarefa(tarefa){
     li.append(paragrafo)
     li.append(botao)
 
+    li.onclick = () => {
+        document.querySelectorAll('.app__section-task-list-item-active')
+            .forEach(elemento => {
+                elemento.classList.remove('app__section-task-list-item-active')
+            })
+        if (tarefaSelecionada == tarefa) {
+            paragrafoDescricaoTarefa.textContent = ''
+            tarefaSelecionada = null
+            liTarefaSelecionada = null
+            return
+        }
+        tarefaSelecionada = tarefa
+        liTarefaSelecionada = li
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao
+        
+        li.classList.add('app__section-task-list-item-active')
+    }
+
     return li
 
 }
@@ -82,3 +104,10 @@ const limparFormulario = () => {
 
 btnCancelarTarefa.addEventListener('click', limparFormulario)
 
+document.addEventListener('FocoFinalizado', () => {
+    if (tarefaSelecionada && liTarefaSelecionada) {
+        liTarefaSelecionada.classList.remove('app__section-task-list-item-active')
+        liTarefaSelecionada.classList.add('app__section-task-list-item-complete')
+        liTarefaSelecionada.querySelector('button').setAttribute('disabled','disabled')
+    }
+})
